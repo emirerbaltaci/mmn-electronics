5cc652336d759f9c0031824e26f2e64ca3e13361
Emir Erbaltacı <emirerb555@gmail.com>
2026-02-06 18:03:57 +0300

    License Updata

--- NCOM/codegen.py
+++ NCOM/codegen.py
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright (c) 2026 Emir Erbaltacı
+# Copyright (c) 2026 MM Nautronics Team
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
