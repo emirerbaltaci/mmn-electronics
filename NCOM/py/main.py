07bb133628c4152cffcb5bf89a084b41fa97a560
Emir Erbaltacı <emirerb555@gmail.com>
2026-02-06 16:07:43 +0300

    NCOM - main.py Comments

--- NCOM/py/main.py
+++ NCOM/py/main.py
@@ -1,4 +1,4 @@
-MAINLOOP_SLEEP = 0.01 # Seconds
+MAINLOOP_SLEEP = 0.1 # Seconds
 
 import serial
 import serial.tools.list_ports
@@ -6,7 +6,7 @@ import time
 import ncom_protocol as ncom
 import ncom_driver as ncd
 
-def find_stm32g4():
+def find_stm32g4(): # Find the STM32G4 COM port by VID and PID
     for port in serial.tools.list_ports.comports():
         if port.vid == 0x0483 and port.pid == 0x5740:
             return port.device
@@ -18,32 +18,25 @@ def main():
     while port == None:
         port = find_stm32g4()
         time.sleep(1)
-    ser = serial.Serial(port, 115200, timeout=0.1)
+    ser = serial.Serial(port, baudrate=115200, timeout=0.1)  # No matter the baudrate, USB CDC runs at full speed
     print("Device connected.")
     
-    parser = ncd.NCOMParser()
-    last_heartbeat = 0.0
-    device_opened = time.time()
+    parser = ncd.NCOMParser()   # Create NCOM parser instance
+    last_hearbeat = 0.0
     
-    while True:
+    while True: # Main loop
         time.sleep(MAINLOOP_SLEEP)
         curr_time = time.time()
-        if curr_time - last_heartbeat >= 1.0:
-            packet = ncd.create_frame(0, 1, 3, 0, int(curr_time - device_opened))
-            if packet:
-                ser.write(packet)
-            last_heartbeat = curr_time
-        
-        if ser.in_waiting:
-            chunk = ser.read(ser.in_waiting)
-            for byte in chunk:
-                packet = parser.parse_byte(byte)
-                if packet:
-                    msg_id, data = packet
-                    handle_packet(msg_id, data)
+        if ser.in_waiting: # Check if there are any bytes in the RX buffer
+            chunk = ser.read(ser.in_waiting)   # Read all available bytes
+            for byte in chunk: # Parse each byte
+                packet = parser.parse_byte(byte) # Get a packet if available
+                if packet:  # If a packet is received
+                    msg_id, data = packet # Unpack the packet
+                    handle_packet(msg_id, data) # Handle the packet
 
 def handle_packet(msg_id, data):
-    if msg_id == 0:
+    if msg_id == ncom.Messages.ID_TO_NAME["HEARTBEAT"]:
         print(f"Device ID: {data[0]}\nMode: {data[1]}\nFlags: {data[2]}\nUptime: {data[3]}")
 
 if __name__ == "__main__":
